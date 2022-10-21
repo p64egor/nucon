62fd4927f45fc71207115a0d75ba842e661c6523
p64egor <76708881+p64egor@users.noreply.github.com>
2022-10-21 14:18:54 -0400

    Update Prover.hpp

--- Prover.hpp
+++ Prover.hpp
@@ -1,3 +1,22 @@
+/*
+nucon. nucon is a program that tries to prove is own consistency.
+Copyright (C) 2022  p64egor
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+	
 #ifndef PROVER_HPP
 #define PROVER_HPP
 
